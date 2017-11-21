f9bfe43cf48da56c7f1edab5f0a363403a68f73b
Andrew Gray <andrew.paul.gray@gmail.com>
2017-11-21 18:39:32 +1100

    Attempt to add a theory to a single test.

--- test/src/test/java/au/com/fullcirclesolutions/saucery/tests/GuineaPigTest.java
+++ test/src/test/java/au/com/fullcirclesolutions/saucery/tests/GuineaPigTest.java
@@ -2,22 +2,49 @@ package au.com.fullcirclesolutions.saucery.tests;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+//import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+//import java.util.Arrays;
+//import java.util.List;
+//import org.junit.runners.Parameterized.Parameter;
+//import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Theories.class)
 public class GuineaPigTest extends SauceryBase {
+    @DataPoints
+   public static int[] ints = { 1, 2, 3, 4, 5 };
+    
+    //@Parameters(name = "{index}: Test with X={0}, Y={1}")
+    //public static List<int[]> data() {
+    //    return Arrays.asList(new int[][] {
+    //             { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }  
+    //       });
+    //}
+    
+    //@Parameter // first data value (0) is default
+    //public /* NOT private */ int x;
 
+    //@Parameter(1)
+    //public /* NOT private */ int y;
+    
     public GuineaPigTest(String os, String platform, String browser, String browserVersion, String longName,
             String longVersion, String url, String device, String deviceOrientation) {
         super(os, platform, browser, browserVersion, longName, longVersion, url, device, deviceOrientation);
     }
     
-    @Test
-    public void PageTitle() {
+    @Theory
+    public void PageTitle(int data) {
+        System.out.println(String.format("I am a data driven test testing with data=%1$d.", data));
+        
         Driver.navigate().to("https://saucelabs.com/test/guinea-pig");
-
+        
         // verify the page title is correct
         Assert.assertTrue(Driver.getTitle().contains("I am a page title - Sauce Labs"));
     }
