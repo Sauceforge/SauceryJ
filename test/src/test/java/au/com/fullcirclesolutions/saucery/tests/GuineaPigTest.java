803eb8399adc213195a629e8cc6d108b2f946763
Andrew Gray <andrew.paul.gray@gmail.com>
2017-11-21 19:48:55 +1100

    Back to parameters

--- test/src/test/java/au/com/fullcirclesolutions/saucery/tests/GuineaPigTest.java
+++ test/src/test/java/au/com/fullcirclesolutions/saucery/tests/GuineaPigTest.java
@@ -1,60 +1,42 @@
 package au.com.fullcirclesolutions.saucery.tests;
 
 import org.junit.Assert;
-//import org.junit.BeforeClass;
 import org.junit.Test;
-//import org.junit.experimental.theories.DataPoints;
-import org.junit.experimental.theories.Theories;
-import org.junit.experimental.theories.Theory;
-import org.junit.experimental.theories.suppliers.TestedOn;
 import org.junit.runner.RunWith;
-//import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-//import java.util.Arrays;
-//import java.util.List;
-//import org.junit.runners.Parameterized.Parameter;
-//import org.junit.runners.Parameterized.Parameters;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(Theories.class)
+@RunWith(Parameterized.class)
 public class GuineaPigTest extends SauceryBase {
-    private static String[] datapoints;
+    
+    @Parameters(name = "{index}: Test with X={0}, Y={1}")
+    public static List<int[]> data() {
+        return Arrays.asList(new int[][] {
+                 { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }  
+           });
+    }
+    
+    @Parameter // first data value (0) is default
+    public /* NOT private */ int x;
 
-    //@BeforeClass
-    //public static void generateData() {
-        // do all the work of generating the datapoints
-    //    datapoints = new String[] { "1", "2", "3", "4", "5" };
-    //}
-    
-    //@DataPoints
-    //public static String[] data() {
-        //return new String[] { "1", "2", "3", "4", "5" };
-    //    return datapoints;
-    //}
-    
-    //@Parameters(name = "{index}: Test with X={0}, Y={1}")
-    //public static List<int[]> data() {
-    //    return Arrays.asList(new int[][] {
-    //             { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }  
-    //       });
-    //}
-    
-    //@Parameter // first data value (0) is default
-    //public /* NOT private */ int x;
-
-    //@Parameter(1)
-    //public /* NOT private */ int y;
+    @Parameter(1)
+    public /* NOT private */ int y;
     
     public GuineaPigTest(String os, String platform, String browser, String browserVersion, String longName,
             String longVersion, String url, String device, String deviceOrientation) {
         super(os, platform, browser, browserVersion, longName, longVersion, url, device, deviceOrientation);
     }
     
-    @Theory
-    public void PageTitle(@TestedOn(ints = {100, 200, 300}) final Integer value) {
-        System.out.println(String.format("I am a data driven test testing with value=%d.", value));
+    @Test
+    public void PageTitle() {
+        System.out.println(String.format("I am a data driven test testing with x=%d and y=%d.", x, y));
         
         Driver.navigate().to("https://saucelabs.com/test/guinea-pig");
         
